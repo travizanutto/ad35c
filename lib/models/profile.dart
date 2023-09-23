149cb9e958ed64e529d0cedd2b368c071c6858eb
Augusto Padilha <fpadilha.augusto@gmail.com>
2023-09-23 20:48:43 -0300

    Adicionado protótipo de Profile Page

---
+++ lib/models/profile.dart
@@ -0,0 +1,17 @@
+class UserProfile {
+  final String id;
+  final String username;
+  final String email;
+  final String phoneNumber;
+  final String bio;
+  final String profileImageUrl;
+
+  UserProfile({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.phoneNumber,
+    required this.bio,
+    required this.profileImageUrl,
+  });
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vwalltet/pages/transaction_controller.dart';
+import 'profile_page.dart';
+import '../models/profile.dart';
 import 'package:vwalltet/pages/transaction_page.dart';
 
 class CustomColor {
@@ -12,13 +14,27 @@ class CustomColor {
   static const EASports = 0xffeaeaea;
 }
 
-class HomePage extends StatelessWidget {
+UserProfile userProfile = UserProfile(
+  id: 'userId',
+  username: 'NomeDeUsuario',
+  email: 'usuario@example.com',
+  phoneNumber: '(123) 456-7890',
+  bio: 'Descrição do usuário',
+  profileImageUrl: 'assets/profile_image.jpg',
+);
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   var transactionController = TransactionController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: homePageAppBar(),
+      appBar: homePageAppBar(context),
       body: ListView.separated(
         itemCount: transactionController.transactionList.length,
         itemBuilder: (BuildContext context, int transaction) {
@@ -43,6 +59,7 @@ class HomePage extends StatelessWidget {
         padding: EdgeInsets.all(0),
       ),
       backgroundColor: const Color(CustomColor.EASports),
+
     );
   }
 }
@@ -67,17 +84,28 @@ Row appBarTitle() {
   );
 }
 
-AppBar homePageAppBar() {
+AppBar homePageAppBar(BuildContext context) {
   return AppBar(
     title: appBarTitle(),
     centerTitle: false,
     backgroundColor: const Color(CustomColor.pompAndPower),
     actions: [
-      Container(
-        margin: const EdgeInsets.all(10),
-        width: 30,
-        height: 30,
-        child: SvgPicture.asset('assets/icons/man_pp.svg'),
+      GestureDetector(
+        onTap: () {
+          // Navegue para a ProfilePage quando o ícone for tocado.
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProfilePage(userProfile: userProfile),
+            ),
+          );
+        },
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          width: 30,
+          height: 30,
+          child: SvgPicture.asset('assets/icons/man_pp.svg'),
+        ),
       ),
     ],
     elevation: 0,
