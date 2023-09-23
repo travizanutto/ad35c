814c6fc9b7b81e11c9c8495f885aafce659bbe8b
travizanutto <ctravizanutto@gmail.com>
2023-09-23 13:50:12 -0300

    transaction_repository

---
+++ lib/repositories/transaction_repository.dart
@@ -0,0 +1,7 @@
+import 'package:vwalltet/models/transaction.dart';
+
+class TransactionRepository {
+  final List<Transaction> _transactionList = [];
+
+  get transactionList => _transactionList;
+}
