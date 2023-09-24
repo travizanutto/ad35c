36b63a3263660ec7ac07f7891718a59b4690283c
Caio Furlan <caiofurlanrufinosilva@gmail.com>
2023-09-23 21:44:29 -0300

    Teste

---
+++ lib/pages/transaction_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:vwalltet/pages/home_page.dart';
+import '../models/transaction.dart';
+
+class TransactionPage extends StatefulWidget {
+  Transaction transaction;
+  TransactionPage({required Key key, required this.transaction}) : super(key: key);
+
+  @override
+  State<TransactionPage> createState() => _TransactionPageState();
+}
+
+class _TransactionPageState extends State<TransactionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController (
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(CustomColor.pompAndPower),
+          title: Text(widget.transaction.name),
+          bottom: TabBar(tabs: [
+            Tab(
+              icon: Icon(Icons.stacked_line_chart),
+              text: 'Informações',
+            ),
+            Tab(
+              icon: Icon(Icons.emoji_events),
+              text: 'blablabla',
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
