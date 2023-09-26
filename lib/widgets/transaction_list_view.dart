e9f31b5afafff35b1c8e7f5e0e5f774617fed6cd
travizanutto <ctravizanutto@gmail.com>
2023-09-26 16:32:55 -0300

    fix minor bug with transactions

--- lib/widgets/transaction_list_view.dart
+++ lib/widgets/transaction_list_view.dart
@@ -8,42 +8,45 @@ import 'package:vwalltet/pages/transaction_edit_page.dart';
 
 class TransactionListView extends StatelessWidget {
   final currencyFormat = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
-  final controller = Get.find<CardController>();
   final int cardIndex;
 
   TransactionListView({super.key, required this.cardIndex});
 
   @override
   Widget build(BuildContext context) {
-    final CardModel card = controller.cardList[cardIndex];
-    return ListView.separated(
-      itemBuilder: (BuildContext context, int index) {
-        final transaction = card.transactionList[index];
-        return ListTile(
-            leading: const SizedBox(
-              width: 40,
-              height: 40,
-              child: Icon(Icons.attach_money),
-            ),
-            title: Text(transaction.name),
-            subtitle: Text(transaction.description),
-            trailing: Text(
-              currencyFormat.format(transaction.price.abs()),
-              style: transaction.price.abs() != transaction.price
-                  ? const TextStyle(color: Colors.red)
-                  : const TextStyle(color: Colors.green),
-            ),
-            onTap: () => Get.to(
-                  TransactionEditPage(cardIndex: cardIndex, transactionIndex: index),
-                ));
-      },
-      separatorBuilder: (_, __) => const Divider(
-        indent: 16,
-        endIndent: 16,
-        thickness: 1,
-        color: Color(CustomColor.delftBlue),
-      ),
-      itemCount: card.transactionList.length,
-    );
+    final controller = Get.put(CardController());
+    return Obx(() {
+      return ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          final CardModel card = controller.cardList[cardIndex];
+          final transaction = card.transactionList[index];
+          return ListTile(
+              leading: const SizedBox(
+                width: 40,
+                height: 40,
+                child: Icon(Icons.attach_money),
+              ),
+              title: Text(transaction.name),
+              subtitle: Text(transaction.description),
+              trailing: Text(
+                currencyFormat.format(transaction.price.abs()),
+                style: transaction.price.abs() != transaction.price
+                    ? const TextStyle(color: Colors.red)
+                    : const TextStyle(color: Colors.green),
+              ),
+              onTap: () => Get.to(
+                    TransactionEditPage(
+                        cardIndex: cardIndex, transactionIndex: index),
+                  ));
+        },
+        separatorBuilder: (_, __) => const Divider(
+          indent: 16,
+          endIndent: 16,
+          thickness: 1,
+          color: Color(CustomColor.delftBlue),
+        ),
+        itemCount: controller.cardList[cardIndex].transactionList.length,
+      );
+    });
   }
 }
